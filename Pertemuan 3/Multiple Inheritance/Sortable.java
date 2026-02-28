d60984210bd61c3e84ebbfc1e0220833ee48e887
bungaaaa <157906406+IvankaBunga@users.noreply.github.com>
2026-02-28 23:50:40 +0700

    Add abstract Sortable class with shell_sort method

---
+++ Pertemuan 3/Multiple Inheritance/Sortable.java
@@ -0,0 +1,8 @@
+abstract class Sortable {
+    // Metode abstrak yang harus diisi oleh subclass
+    public abstract int compare(Sortable b);
+
+    public static void shell_sort(Sortable[] a) {
+        // Shell sort body
+    }
+}
